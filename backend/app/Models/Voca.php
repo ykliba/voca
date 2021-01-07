0b43333b5a045e03678de3bf0defecf63c47050e
ykliba <mediaboxing2@gmail.com>
2021-01-07 10:36:03 +0900

    voca table

---
+++ backend/app/Models/Voca.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Voca extends Model
+{
+    public function users() {
+        return $this->hasMany('App\Models\User'); 
+    }
+}
